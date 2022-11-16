b6ebb767cb2cf8ae8b88b7f8b211aef27e51a02b
alessiocremo <alessiocremo@DESKTOP-L55JODL.localdomain>
2022-11-16 19:31:34 +0200

    controller intialized

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,12 @@
+const getGoals = (req, res) => {
+    res.status(200).json({message: 'Get goals'});
+}
+
+module.exports = {
+    getGoals/*,
+    postGoals,
+    putGoals,
+    deleteGoals*/
+}
+
+//you arrived at 22:00
